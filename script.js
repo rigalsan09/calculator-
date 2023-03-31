68e9feaab48b54bf25a24f3130575efcf6ed54de
rigalsan09 <rigallegos09@gmail.com>
2023-03-30 23:53:06 -0700

    Front end design complete using grid and flexbox

---
+++ script.js
@@ -0,0 +1,7 @@
+/*
+Your calculator is going to contain functions for all of the basic math operators you typically find on simple calculators, so start by creating functions for the following items and testing them in your browser’s console.
+add
+subtract
+multiply
+divide
+ */
